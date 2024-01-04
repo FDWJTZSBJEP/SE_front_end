17f5bb8b00ca2f6f06fbf298740100670b16ed0f
zhou dao <1422024869@qq.com>
2024-01-04 22:34:28 +0800

    修改样式

--- src/components/layout/AppLayout.vue
+++ src/components/layout/AppLayout.vue
@@ -55,6 +55,7 @@ import AppAside from "./AppAside.vue";
     padding: 20px;
     box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
     width: 200px; /* 调整左侧导航栏的宽度 */
+    overflow: hidden;
   }
 
   .el-main {
--- src/views/AnalyzeView.vue
+++ src/views/AnalyzeView.vue
@@ -1,45 +1,42 @@
 <template>
   <el-tabs type="border-card">
-    <el-tab-pane label="语言时间">
+    <el-tab-pane label="语言时间" >
       <div v-if="TimeLanguageData.length === 0" class="loading-message">暂无数据</div>
-      <div id="language-time-bar-chart" style="width: 1400px; height: 700px;"></div>
+      <div id="language-time-bar-chart" style="width: 100%; height: 600px;"></div>
+      <div ref="TimeLanguageLineContainer" style="width: 100%; height: 600px;"></div>
+    </el-tab-pane>
 
+    <el-tab-pane label="语言项目">
+      <div id="bar-chart" style="width: 600px; height: 700px;"></div>
     </el-tab-pane>
 
     <el-tab-pane label="国家用户">
       <div>
         <h1>国家用户分析图</h1>
-        <div id="pie-chart" style="width: 800%; height: 400px;"></div>
+        <div id="pie-chart" style="width: 800%; height: 700px;"></div>
       </div>
-    </el-tab-pane>
-
-    <el-tab-pane label="语言项目">
-      <div id="bar-chart" style="width: 600px; height: 400px;"></div>
     </el-tab-pane>
 
     <el-tab-pane label="国家粉丝">
       <!-- 社区成员的内容 -->
     </el-tab-pane>
 
-    <el-tab-pane label="时间段操作数">
-      <!-- 社区成员的内容 -->
+    <el-tab-pane label="时间段操作与提交">
+      <div ref="TimeOperateCommitChartContainer" style="width: 1350px; height: 600px;"></div>
     </el-tab-pane>
 
-    <el-tab-pane label="语言收藏量">
-      <!-- 社区成员的内容 -->
+    <el-tab-pane label="语言收藏">
+      <h1>语言收藏量分析图</h1>
+      <div id="star-language-chart" style="width: 1000px; height: 600px;"></div>
+      <p :style="{ fontSize: '20px', color: '#333', fontWeight: 'bold' }">总收藏数: {{ totalStarredNumRef }}</p>
     </el-tab-pane>
 
-    <el-tab-pane label="提交数量与时间">
-      <!-- 社区成员的内容 -->
-    </el-tab-pane>
-
-    <el-tab-pane label="语言查阅量">
-      <!-- 社区成员的内容 -->
+    <el-tab-pane label="语言查阅">
+      <h1>语言查阅量分析图</h1>
+      <div id="watch-language-chart" style="width: 1000px; height: 600px;"></div>
     </el-tab-pane>
   </el-tabs>
 </template>
-
-
 
 <script setup lang="ts">
 import request from "@/utils/request";
@@ -51,12 +48,13 @@ const barChartInstance = ref<echarts.ECharts | null>(null);
 
 const responseData2 = ref<any[]>([]);
 const responseData3 = ref<any[]>([]);
-
+const totalStarredNumRef = ref<number>(0);
 
 
 //时间语言相关开始
 const languageTimeBarChartInstance = ref<echarts.ECharts | null>(null);
 const TimeLanguageData = ref<any[]>([]);
+const TimeLanguageLineContainer = ref(null)
 const getTimeLanguageData = async () => {
   try {
     const response = await request({
@@ -84,7 +82,7 @@ const renderLanguageTimeBarChart = () => {
 
   const option = {
     title: {
-      text: `语言使用情况 (${TimeLanguageData.value[0].time})`,
+      text: `语言使用情况 (${TimeLanguageData.value[0].time.split(' ')[0]})`,
       left: 'center'
     },
     tooltip: {
@@ -97,7 +95,7 @@ const renderLanguageTimeBarChart = () => {
       type: 'category',
       data: languages,
       axisLabel: {
-        rotate: 45, // 旋转标签
+        rotate: 30, // 旋转标签
         interval: 0, // 显示所有标签
       }
     },
@@ -124,7 +122,7 @@ const renderLanguageTimeBarChart = () => {
   }
 };
 
-const sortAndUpdateChart = (index) => {
+const sortAndUpdateChart = (index:number) => {
   // 获取当前数据点并进行排序
   const currentData = TimeLanguageData.value[index];
   const sortedData = Object.entries(currentData)
@@ -139,7 +137,7 @@ const sortAndUpdateChart = (index) => {
   // 更新图表
   languageTimeBarChartInstance.value?.setOption({
     title: {
-      text: `语言使用情况 (${TimeLanguageData.value[index].time})`,
+      text: `语言使用情况 (${TimeLanguageData.value[index].time.split(' ')[0]})`,
       left: 'center'
     },
     xAxis: {
@@ -159,19 +157,148 @@ const startAutoPlay = () => {
   }, 3000); //每三秒变化
 };
 
+const renderTimeLanguageLineChart = () =>{
+  const dates = TimeLanguageData.value.map(item => item.time.split(' ')[0]);
+// 提取所有语言
+  const languages = Object.keys(TimeLanguageData.value[0]).filter(key => key !== 'time');
+
+// 为每种语言创建一个系列
+  const series = languages.map(language => {
+    return {
+      name: language,
+      type: 'line',
+      data: TimeLanguageData.value.map(item => item[language]),
+      smooth:true
+    };
+  });
+
+  const option = {
+    tooltip: {
+      trigger: 'axis'
+    },
+    dataZoom: {
+      type:'inside'
+    },
+    legend: {
+      data: languages
+    },
+    xAxis: {
+      type: 'category',
+      data: dates
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: series
+  };
+
+// 初始化并设置选项
+  const lineChart = echarts.init(TimeLanguageLineContainer.value);
+  lineChart.setOption(option);
+
+}
+
 onMounted(() => {
   getTimeLanguageData();
   startAutoPlay();
+
 });
 
 watch(TimeLanguageData, () => {
   renderLanguageTimeBarChart();
+  renderTimeLanguageLineChart();
 });
 
 //时间语言相关结束
 
+//时间操作数和时间提交数开始
+const TimeOperateCommitChartContainer = ref(null);
+const TimeOperateCommitData = ref<any[]>([])
+const getTimeOperateCommitData = async () => {
+  try {
+    const response = await request({
+      method: "GET",
+      url: "https://mock.apifox.com/m1/3807087-0-default/TimeOperateCommit",
+    });
+    TimeOperateCommitData.value = response.data.data || [];
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+
+const renderTimeOperateCommitChart= () =>{
+  if(TimeOperateCommitData.value.length === 0) return;
+  const TimeOperateCommitChart = echarts.init(TimeOperateCommitChartContainer.value)
+  const dates = TimeOperateCommitData.value.map(item => item.time.split(' ')[0])
+  const operates = TimeOperateCommitData.value.map((item => item.operate))
+  const commits = TimeOperateCommitData.value.map((item => item.commit))
+
+  const option = {
+    legend: {
+      data:["operates","commits"]
+    },
+    tooltip: {
+      trigger: 'axis', // 或者可以是'item'，根据需要选择
+      formatter: function(params:any) {
+        // 定制你想显示的内容
+        let result = params[0].name + '<br/>'; // 显示横坐标值
+        params.forEach(function (item:any) {
+          result += item.marker + " " + item.seriesName + ": " + item.value + '<br/>';
+        });
+        return result;
+      }
+    },
+    xAxis:{
+      type:'category',
+      data:dates
+    },
+    yAxis:{
+      type:'value'
+    },
+    series:[{
+      type:'line',
+      data:operates,
+      smooth:true,
+      name:'operates'
+    },
+      {
+        type:'line',
+        data:commits,
+        smooth:true,
+        name:'commits',
+        // 设置线条样式
+        lineStyle: {
+          color: '#ff0000',
+          width: 2,
+        },
+        // 设置拐点样式
+        itemStyle: {
+          color: '#ff0000',
+        }
+      }
 
 
+    ]
+  }
+  TimeOperateCommitChart.setOption(option)
+
+};
+
+onMounted(()=>{
+  getTimeOperateCommitData()
+
+}
+)
+
+watch(TimeOperateCommitData,(newVal)=>{
+  if(newVal && newVal.length > 0)
+    renderTimeOperateCommitChart();
+    }
+)
+//时间操作数结束
+
+
+// 国家用户部分开始
 const fetchData2 = async () => {
   try {
     const response = await request({
@@ -217,11 +344,15 @@ const renderPieChart = () => {
       },
     }],
   };
-
-  pieChartInstance.value = echarts.init(document.getElementById('pie-chart'))!;
+  const chartDom = document.getElementById('pie-chart');
+  if (chartDom){
+  pieChartInstance.value = echarts.init(chartDom);
   pieChartInstance.value.setOption(option);
+  }
 };
+// 国家用户部分结束
 
+// 语言项目部分开始
 const fetchData3 = async () => {
   try {
     const response = await request({
@@ -234,8 +365,6 @@ const fetchData3 = async () => {
     console.error('Error fetching data:', error);
   }
 };
-
-
 
 const renderBarChart = () => {
   const xAxisData = responseData3.value.map((data) => data.language);
@@ -305,10 +434,220 @@ const renderBarChart = () => {
   barChartInstance.value.setOption(option);
 };
 
-
+// 语言项目部分结束
 onMounted(fetchData2);
 onMounted(fetchData3);
 
+
+// 语言收藏量部分开始
+const languageStarChart = ref<echarts.ECharts | null>(null);
+const StarLanguageData = ref<any[]>([]);
+const getStarLanguageData = async () => {
+  try {
+    const response = await request({
+      method: "GET",
+      url: "https://mock.apifox.com/m1/3807087-0-default/language_star",
+    });
+    StarLanguageData.value = response.data.data || [];
+    renderlanguageStarChart();
+    // console.log(StarLanguageData.value);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+const renderlanguageStarChart = () => {
+  const legendData = StarLanguageData.value.map((data) => data.language);
+  const seriesData = StarLanguageData.value.map((data) => ({ name: data.language, value: data.starred_num }));
+  // 计算 seriesData 中 starred_num 的总和
+  const totalStarredNum = seriesData.reduce((accumulator, currentValue) => accumulator + currentValue.value, 0);
+  // 存储在 ref 中
+  totalStarredNumRef.value = totalStarredNum;
+  // console.log('Total Starred Num:', totalStarredNumRef.value);
+
+  const option = {
+  title: {
+    text: '语言收藏量饼图',
+    left: 'center',
+  },
+  legend: {
+    type: 'scroll',
+    orient: 'vertical',
+    right: 100,
+    top: 20,
+    bottom: 20,
+    data: legendData,
+  },
+  series: [
+    {
+      name: '编程语言',
+      type: 'pie',
+      selectedMode: 'single',
+      radius: [0, '30%'],
+      label: {
+        position: 'inner',
+        fontSize: 14,
+      },
+      labelLine: {
+        show: false,
+      },
+      data: seriesData,
+    },
+    {
+      name: '编程语言',
+      type: 'pie',
+      radius: ['45%', '60%'],
+      labelLine: {
+        length: 50,
+      },
+      label: {
+        formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}:}{c}  {per|{d}%}  ',
+        backgroundColor: '#F6F8FC',
+        borderColor: '#8C8D8E',
+        borderWidth: 1,
+        borderRadius: 4,
+        rich: {
+          a: {
+            color: '#6E7079',
+            lineHeight: 22,
+            align: 'center',
+          },
+          hr: {
+            borderColor: '#8C8D8E',
+            width: '100%',
+            borderWidth: 1,
+            height: 0,
+          },
+          b: {
+            color: '#4C5058',
+            fontSize: 14,
+            fontWeight: 'bold',
+            lineHeight: 33,
+          },
+          per: {
+            color: '#fff',
+            backgroundColor: '#4C5058',
+            padding: [3, 4],
+            borderRadius: 4,
+          },
+        },
+      },
+      data: seriesData,
+    },
+  ],
+};
+
+
+  languageStarChart.value = echarts.init(document.getElementById('star-language-chart'))!;
+  languageStarChart.value.setOption(option);
+};
+
+onMounted(getStarLanguageData);
+// 语言收藏量部分结束
+
+// 语言查阅量部分开始
+const languageWatchChart = ref<echarts.ECharts | null>(null);
+const WatchLanguageData = ref<any[]>([]);
+const getWatchLanguageData = async () => {
+  try {
+    const response = await request({
+      method: "GET",
+      url: "https://mock.apifox.com/m1/3807087-0-default/language_watch",
+    });
+    WatchLanguageData.value = response.data.data || [];
+    renderlanguageWatchChart();
+    // console.log(WatchLanguageData.value);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+};
+const renderlanguageWatchChart = () => {
+  const legendData = WatchLanguageData.value.map((data) => data.language);
+  const seriesData = WatchLanguageData.value.map((data) => ({ name: data.language, value: data.watch_num }));
+  // 计算 seriesData 中 starred_num 的总和
+  const totalStarredNum = seriesData.reduce((accumulator, currentValue) => accumulator + currentValue.value, 0);
+  // 存储在 ref 中
+  totalStarredNumRef.value = totalStarredNum;
+  // console.log('Total Starred Num:', totalStarredNumRef.value);
+
+  const option = {
+  title: {
+    text: '语言查阅量饼图',
+    left: 'center',
+  },
+  legend: {
+    type: 'scroll',
+    orient: 'vertical',
+    right: 100,
+    top: 20,
+    bottom: 20,
+    data: legendData,
+  },
+  series: [
+    {
+      name: '编程语言',
+      type: 'pie',
+      selectedMode: 'single',
+      radius: [0, '30%'],
+      label: {
+        position: 'inner',
+        fontSize: 14,
+      },
+      labelLine: {
+        show: false,
+      },
+      data: seriesData,
+    },
+    {
+      name: '编程语言',
+      type: 'pie',
+      radius: ['45%', '60%'],
+      labelLine: {
+        length: 50,
+      },
+      label: {
+        formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}:}{c}  {per|{d}%}  ',
+        backgroundColor: '#F6F8FC',
+        borderColor: '#8C8D8E',
+        borderWidth: 1,
+        borderRadius: 4,
+        rich: {
+          a: {
+            color: '#6E7079',
+            lineHeight: 22,
+            align: 'center',
+          },
+          hr: {
+            borderColor: '#8C8D8E',
+            width: '100%',
+            borderWidth: 1,
+            height: 0,
+          },
+          b: {
+            color: '#4C5058',
+            fontSize: 14,
+            fontWeight: 'bold',
+            lineHeight: 33,
+          },
+          per: {
+            color: '#fff',
+            backgroundColor: '#4C5058',
+            padding: [3, 4],
+            borderRadius: 4,
+          },
+        },
+      },
+      data: seriesData,
+    },
+  ],
+};
+
+
+  languageWatchChart.value = echarts.init(document.getElementById('watch-language-chart'))!;
+  languageWatchChart.value.setOption(option);
+};
+
+onMounted(getWatchLanguageData);
+// 语言查阅量部分结束
 </script>
 
 <style>
